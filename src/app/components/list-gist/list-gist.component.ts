4b1eed3f3cd1f66940be8eccfdc04e298286dba5
Daniella Mato <dmpire2@gmail.com>
2018-03-08 21:08:49 +0100

    Worked on outputing user gist

--- src/app/components/list-gist/list-gist.component.ts
+++ src/app/components/list-gist/list-gist.component.ts
@@ -9,9 +9,21 @@ import { Gist } from '../../models/gist';
 })
 export class ListGistComponent implements OnInit {
 
+  user: Array<any>;
+  usergist: Array<any>;
   gistList: Array<Gist>;
 
-  constructor(private _githubService: GithubService) { }
+  constructor(private _githubService: GithubService) {
+    this._githubService.getUser().subscribe(user => {
+      // console.log(user);
+      this.user = user;
+    });
+
+    this._githubService.getUserGist().subscribe(usergist => {
+      // console.log(usergist);
+      this.usergist = usergist;
+    });
+  }
 
   ngOnInit() {
     this._githubService.getUserGist().subscribe(res => {
